d662d4081f3153143e9b1921a001d11a6ccc51f0
Dennis Urtubia <dennis.urtubia@gmail.com>
2022-04-09 18:02:01 -0300

    chore: gem init

---
+++ spec/license_conflicts_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe LicenseConflicts do
+  it "has a version number" do
+    expect(LicenseConflicts::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
