ca3e352fcf1d8fcce405d921706950f9527b1fa7
Dennis Urtubia <dennis.urtubia@gmail.com>
2022-05-03 22:20:53 -0300

    feat(bin): adds executable file to license_conflicts execution

--- lib/license_conflicts.rb
+++ lib/license_conflicts.rb
@@ -1,8 +1,7 @@
 # frozen_string_literal: true
 
-require_relative "license_conflicts/version"
+require_relative 'license_conflicts/finder'
+require_relative 'license_conflicts/version'
 
 module LicenseConflicts
-  class Error < StandardError; end
-  # Your code goes here...
 end
