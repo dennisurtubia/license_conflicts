e6b447ef55286b5dc9eacceb244b03e5db2ad8a4
Dennis Urtubia <dennis.urtubia@gmail.com>
2022-06-01 20:31:14 -0300

    lala

--- lib/license_conflicts/finder.rb
+++ lib/license_conflicts/finder.rb
@@ -5,38 +5,65 @@ require 'license_conflicts/conflicts_map'
 
 module LicenseConflicts
   class Finder
-    attr_reader :config, :main_license, :unapproved, :finder, :project_name
+    attr_reader :config, :main_license, :unapproved, :finder, :package_json
 
-    def initialize(project_name)
-      @project_name = project_name
+    def initialize
       @config ||= LicenseFinder::Configuration.with_optional_saved_config(license_finder_config)
     end
   
     def find_conflicts
-      examinated_package = unapproved.find { |d| d.name == project_name }
-      @main_license = examinated_package.licenses.first.name
-  
+      @main_license = project_license
+
+      raise Exception.new 'license_not_found' if main_license.nil?
+      raise Exception.new "examinated_package_license_not_mapped_#{main_license}" unless LicenseConflicts::CONFLICTS_MAP.has_key?(main_license)
+
       check_conflicts
     end
-  
-    private
-  
-    def license_finder_config
-      { prepare: true }
+
+    def dependencies_count
+      unapproved.count - 1
     end
-  
+
+    def project_license
+      examinated_package = unapproved.find { |d| d.name == project_name }
+      license_finder_license_found = examinated_package.licenses.first.name unless examinated_package.nil?
+
+      license_finder_license_found || package_json['license']
+    end
+
+    private
+
+    def project_name
+      package_json['name'] || Pathname.pwd.basename.to_s
+    end
+
+    def package_json
+      file = File.open('./package.json', 'r')
+
+      @package_json ||= JSON.parse(file.read)
+    ensure
+      file.close
+    end
+
+    def license_finder_config
+      {
+        prepare: true,
+        logger: LicenseFinder::Logger::MODE_QUIET
+      }
+    end
+
     def finder
       @finder ||= LicenseFinder::LicenseAggregator.new(config, nil)
     end
-  
+
     def unapproved
       @unapproved ||= finder.unapproved
     end
-  
+
     def check_conflicts
       unapproved.filter { |dependency| has_conflict?(dependency.licenses.first.name) }
     end
-  
+
     def has_conflict?(dependency_license)
       LicenseConflicts::CONFLICTS_MAP[main_license].include?(dependency_license)
     end
